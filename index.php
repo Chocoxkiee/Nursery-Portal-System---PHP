42b25d5aebd63c730efcf83c072d0a42e165a0ed
Ciell <141306613+Chocoxkiee@users.noreply.github.com>
2026-03-07 20:52:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,334 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>E-commerce</title>
+</head>
+<body>
+<div class="header">
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <a><h1>Green<span>Bloom</span></h1></a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="../ECOMMERCE/index.php">Home</a></li>
+                    <li><a href="./pages/plants.html">Plants</a></li>
+                    <li><a href="./pages/about.html" class="active">About</a></li>
+                    <li><a href="./pages/contact.html">Contact</a></li>
+                    
+                </ul>
+            </nav>
+            <img src="..images/cart.png" width="30px" height="30px">
+        </div>
+        <div class="row">
+            <div class="col-1">
+                <h1>Give Your Workout<br><br>A New Style!</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>Sapiente in esse animi quos, dicta ad!</p>
+                <a href="" class="btn">Explore now &#8594</a>
+            </div>
+            <div class="col-2">
+                <img src="../images/vecteezy_rock-garden-with-purple-flowers-isolated-on-a-transparent_47920956.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- ----------- featured categories ------------ -->
+    <div class="categories">
+        <div class="small-container">
+            <h1>Plant Categories</h1>
+            <p>Explore our range of houseplants, succulents, and flowering plants.</p>
+            <div class="row">
+                <div class="col ">
+                    <img src="../images/1_19048889-bb30-410f-b2eb-ce5f639d159e.webp" >
+                    <h3>INDOOR<span> PLANTS...</span></h3>
+                </div>
+                <div class="col ">
+                    <img src="../images/1_87180b78-9408-426a-a51d-0c4f75335159.webp" >
+                    <h3>SUCCU<span>LENTS...</span></h3>
+                </div>
+                <div class="col ">
+                    <img src="../images/1_a13c280d-c164-4511-bdc2-1a9a237f01ea.webp" >
+                    <h3>FLOWE<span>RING...</span></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- -------------- featured products ----------- -->
+
+    <div class="small-container">
+        <h2 class="title">FEATURED PLANTS</h2>
+        <p>Discover beautiful, low-maintenance plants to liven up your space.</p>
+        <div class="row">
+            <div class="col-4">
+                <img src="../images/Aimage_a874260c-0dc8-4759-b34a-fe3c9f1a6374.webp">
+                <h4>Areca Palm</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
+                </div>
+                <p>$150.00</p>
+            </div>
+            <div class="col-4">
+                <img src="../images/ixora-rugmini-plant-yellow-gift-hamper-32173726793860.webp">
+                <h4>Snake Plant</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$100.00</p>
+            </div>
+            <div class="col-4">
+                <img src="../images/nurserylive-combo-packs-plants-5-best-fragrant-plants-16968509653132_210x210.webp">
+                <h4>ZZ Plant</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
+                </div>
+                <p>$130.00</p>
+            </div>
+            <div class="col-4">
+                <img src="../images/nurserylive-g-hyacinth-random-color-flower-bulbs-257855_224x224.webp">
+                <h4>Peace Lily</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>$180.00</p>
+            </div>
+        </div>
+        <h2 class="title">LATEST PLANTS</h2>
+        <div class="row">
+            <div class="col-4">
+                <img src="../images/1_a13c280d-c164-4511-bdc2-1a9a237f01ea.webp">
+                <h4>Spider Plant</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
+                </div>
+                <p>$150.00</p>
+            </div>
+            <div class="col-4">
+                <img src="../images/1_e93a0814-f344-4f3f-9161-cfa5704c5e0e.webp">
+                <h4>Jade Plant</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$100.00</p>
+            </div>
+            <div class="col-4">
+                <img src="../images/xl_in_round_pot_-031.webp">
+                <h4>Rubber Plant</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
+                </div>
+                <p>$130.00</p>
+            </div>
+            <div class="col-4">
+                <img src="../images/peace-lily-plant-set-of-2-31818398957700.webp">
+                <h4>Fiddle Leaf Fig</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>$180.00</p>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <img src="../images/1_87180b78-9408-426a-a51d-0c4f75335159.webp">
+                    <h4></h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half"></i>
+                    </div>
+                    <p>$150.00</p>
+                </div>
+                <div class="col-4">
+                    <img src="../images/1_19048889-bb30-410f-b2eb-ce5f639d159e.webp">
+                    <h4></h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$100.00</p>
+                </div>
+                <div class="col-4">
+                    <img src="../images/1_a13c280d-c164-4511-bdc2-1a9a237f01ea.webp">
+                    <h4>Courata</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half"></i>
+                    </div>
+                    <p>$130.00</p>
+                </div>
+                <div class="col-4">
+                    <img src="../images/1_e93a0814-f344-4f3f-9161-cfa5704c5e0e.webp">
+                    <h4>Hazaamal</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <p>$180.00</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- --------------- offer --------------- -->
+
+    <div class="offer">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-2">
+                    <img src="../images/last.png" class="offer-img">
+                </div>
+                <div class="col-2">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <h1>Plant Starter Kit</h1>
+                    <small>Includes potting soil, fertilizer, and care guide for beginners.</small>
+                    <a href="" class="btn">Buy Now &#8594</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- --------------------- testimonials -------------- -->
+
+    <div class="testimonial">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>"This plant store is amazing! The delivery was fast and the plants arrived healthy."</p>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <img src="images/user-1.png" alt="">
+                    <h3>Dimpal <span>Parmar</span></h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>"This plant store is amazing! The delivery was fast and the plants arrived healthy."</p>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <img src="images/user-1.png" alt="">
+                    <h3>Rajvi<span>Manvar</span></h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>"This plant store is amazing! The delivery was fast and the plants arrived healthy."</p>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <!-- <i class="fa fa-star"></i> -->
+                    </div>
+                    <img src="images/user-1.png" alt="">
+                    <h3>Ashika<span>Iffadia</span></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- -------------- footer -------------------- -->
+
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 footer-col-1">
+                    <h3>Download Our App</h3>
+                    <p>Download App for Android and ios Mobile Phone.</p>
+                    <div class="app-logo">
+                        <img src="images/play-store.png" alt="">
+                        <img src="images/app-store.png" alt="">
+                    </div>
+                </div>
+                <div class="col-6 footer-col-2">
+                    <a class="f-logo">
+                        <h1>Green<span>Bloom</span></h1>
+                    </a>
+                    <p>We bring nature to your doorstep. Shop from our collection of healthy, air-purifying plants!</p>
+                </div>
+                <div class="col-6 footer-col-3">
+                    <h3>Useful Links</h3>
+                    <ul>
+                        <li> Coupons</li>
+                        <li> Blog Post</li>
+                        <li> Return Policy</li>
+                        <li> Join Affiliates</li>
+                    </ul>
+                </div>
+                <div class="col-6 footer-col-4">
+                    <h3>Follow Us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright">copyright-reserved  @Jay Goswami</p>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
